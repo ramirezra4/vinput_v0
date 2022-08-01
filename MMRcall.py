44aaa092715917766b25a2606dbd60206c9bee19
ramirezra4 <51302686+ramirezra4@users.noreply.github.com>
2022-08-01 09:59:44 -0700

    Refactored Platterfunc and dependencies. Cleaned unnessecary calls.

--- MMRcall.py
+++ MMRcall.py
@@ -1,7 +1,4 @@
-from cgi import print_arguments
 from datetime import date, datetime
-from email import header
-from bitarray import test
 import requests
 import pandas as pd
 
@@ -15,17 +12,12 @@ class MMRapi:
 
     API Call follows the format below: [Base_URL][DateTime_String][...][VIN].
     As such a large portion of this module is dedicated to formatting that URL.
-
-    Outputs a dictionary of body: vid pairings.
     """
-
-    def __init__(self, vin):
-        self.vin = vin
 
     # public url
     base_url = f"http://cloud.jdpower.ai/data-api/valuationservices/valuation/vehiclesByVin"
     
-    # private key: security measures needed for production FIXME
+    # private key: security measures needed for production
     api_key = 'ee9b1fc6-f3fc-4f14-ae88-2fcb02d2e3ab'
 
     # datetime object
@@ -37,12 +29,30 @@ class MMRapi:
     # datetime string passed into url
     dt_string = f'{dt_string_partial}-01'
 
-    # final url
-    url = ''
+    _match = ""
+
+    _my = ""
+    """Car Model Year"""
     
+    _make = ""
+    """Car make"""
+
+    _model = ""
+    """Car model"""
+
+    _body = ""
+    """Body style"""
+
+    _ugcID = ""
+    """UGC VID"""
+
+    _vid = ""
+    """Vehicle ID"""
+
     def __init__(self, vin):
+        """Initialize MMR API Call."""
         self.vin = vin
-        self.url
+
 
     # Request MMRAPI, output JSON
     def match(self):
@@ -57,37 +67,44 @@ class MMRapi:
             self.url = f'{self.base_url}?period=0&vin={self.vin}'
             print(self.url)
             r = requests.get(self.url, headers={"api-key": f'{self.api_key}', "accept": "application.json"})
-            return r.json()['result'][0]
+            self._match = r.json()['result'][0]
+            self._my = self._match['modelyear']
+            self._make = self._match['make']
+            self._model = self._match['model']
+            self._body = self._match['body']
+            self._ugc_vid = self._match['ucgvehicleid']
+            self._vid = self._match['vid']
         except:
-            return "MMR API Call Failed."
+            print("MMR API Call Failed.")
 
     def model_year(self):
         """Retrieve Model Year."""
-        return self.match()['modelyear']
+        return self._my
 
     def make(self):
         """Retrieve make."""
-        return self.match()['make']
+        return self._make
 
     def model(self):
         """Retrieve car model."""
-        return self.match()['model']
+        return self._model 
 
     def body(self):
         """Retrieve body style."""
-        return self.match()['body']
+        return self._body 
     
     def ugc_vid(self):
         """
         Retrieve UGC Vehicle ID 
         for some reason not readily apparent.
         """
-        return self.match()['ucgvehicleid']
+        return self._ugcID
 
     def vid(self):
         """You want the VID? Take it!"""
-        return self.match()['vid']
+        return self._vid
 
 ilx = MMRapi('5UXCR6C09N9M97942')
 
-print(ilx.match())
+ilx.match()
+print(ilx._match)
--- Platterfunc.py
+++ Platterfunc.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from MMRcall import MMRapi
 from Cnx import Cnx
-import make as mk
 from fuzzywuzzy import fuzz
+import make as mk
 
 """
 Platterfunc takes in a batch of VIN numbers and serves up
@@ -20,55 +20,26 @@ def main(vin):
     """
     conn = Cnx()
     mmr = MMRapi(vin)
+    mmr.match()
     make = mmr.make()
     bod = mmr.body()
     model = mmr.model()
     year = mmr.model_year()
     """Access MMR Api"""
     slice_bymake = f"""
-    SELECT [ALGResidualNewID]
-    ,[ModelYear]
-    ,[MakeNumber]
-    ,[ModelNumber]
-    ,[Style]
-    ,[ModelDesc]
-    ,[Description]
-    ,[Month24]
-    ,[Month30]
-    ,[Month36]
-    ,[Month42]
-    ,[Month48]
-    ,[Month54]
-    ,[Month60]
-    FROM [vinput].[dbo].[ALGResidualNewTable20220708]
+    SELECT [ALGResidualNewID] ,[ModelYear] ,[MakeNumber] ,[ModelNumber] ,[Style] ,[ModelDesc] ,[Description] ,[Month24] ,[Month30] ,[Month36] ,[Month42] ,[Month48] ,[Month54] ,[Month60] FROM [vinput].[dbo].[ALGResidualNewTable20220708]
     WHERE MakeNumber = {mk.make_number[make]} AND
     ModelYear = {year}
     """
     conn.set_query(slice_bymake)
     make_match = pd.DataFrame(conn.execute())
     make_df = make_match
-    make_df.columns = [
-        'ALGResidualNewID',
-        'ModelYear',
-        'MakeNumber',
-        'ModelNumber',
-        'Style',
-        'ModelDesc',
-        'Description',
-        'Month24',
-        'Month30',
-        'Month36',
-        'Month42',
-        'Month48',
-        'Month54',
-        'Month60'
-        ]
+    make_df.columns = [ 'ALGResidualNewID', 'ModelYear', 'MakeNumber', 'ModelNumber', 'Style', 'ModelDesc', 'Description', 'Month24', 'Month30', 'Month36', 'Month42', 'Month48', 'Month54', 'Month60' ]
     match_index = -1
     match_max = -1
     make_df['FuzzyModel'] = make_df['ModelDesc']
     for i in range(make_df['FuzzyModel'].size):
         make_df['FuzzyModel'][i] = fuzz.partial_ratio(make_df['FuzzyModel'][i], model)
-    
 
     make_df['Fuzzy'] = make_df['Description']
     for i in range(make_df['Fuzzy'].size):
@@ -76,18 +47,8 @@ def main(vin):
         if make_df['Fuzzy'][i] > match_max and make_df['FuzzyModel'][i] > 70:
             match_index = i
             match_max = make_df['Fuzzy'][i]
-    
-    return make_df.iloc[match_index]
 
-    
-def to_df(data):
-    lst = []
-    for i in data:
-        for j in i:
-            lst.append(j)
-    df = pd.DataFrame(lst)
-    df_t = df.transpose()
-    return df_t
+    return make_df.iloc[match_index]
 
 jeepers = main('5UXCR6C09N9M97942')
 print(jeepers)
