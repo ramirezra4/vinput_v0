b4306edfb8e8c15199e64681dd30d7d72392c07e
ramirezra4 <51302686+ramirezra4@users.noreply.github.com>
2022-08-09 09:40:55 -0700

    repo

--- ABSPlatter.py
+++ ABSPlatter.py
@@ -1,9 +1,5 @@
 # import necessary libraries
 from abc import abstractmethod
-import pandas as pd
-import numpy as np
-import pypyodbc as dbc
-import requests as api
 
 class Platter:
     """
@@ -26,57 +22,6 @@ class Platter:
     and view at a high level the chain of logic connecting each of the many dependent sources
     to each other to achieve the desired output.
     """
-
-    @abstractmethod
-    def get_vin():
-        """
-        Get a vin or batch of vins. Dresses up batch into useable internal data structure.
-        """
-        return
-    
-    @abstractmethod
-    def hit_cula():
-        """
-        Check if there is an existing mapping (VIN -> RV) in CULA database.
-        Returns BOOL (True/False).
-        """
-        return
-    
-    @abstractmethod
-    def hit_mmr():
-        """
-        Attempts to map VIN to NADA ID via MMR api request in the event there
-        is no existing mapping through CULA.
-        Returns string = NADA ID#.
-        """
-        return
-    
-    @abstractmethod
-    def map_nada():
-        """
-        Attempts to map NADA ID# to RV table in CULA DB. Could augment HIT_CULA().
-        Returns RV dataframe.
-        """
-        return
-
-    @abstractmethod
-    def is_used():
-        """Returns BOOL indicating whether car (or car in batch) is used."""
-        return
-
-    @abstractmethod
-    def hit_carfax():
-        """
-        Hits carfax api to retrieve history on cars that are mapped to RV's.
-        Implements logic to adjust RV's depending on factors contained in title.
-        """
-        return
-
-    @abstractmethod
-    def rv_out():
-        """Converts mapped RV(s) into desired output format."""
-        return
-    
     @abstractmethod
     def platter():
         """Outputs raw RV's."""
--- configuration.md
+++ configuration.md
@@ -8,9 +8,10 @@
 * pandas
 
 ## Checklist
-1. Install all dependencies.
-2. Open [cnx.py](Cnx.py) and change DRIVER_NAME, SERVER_NAME, DATABASE_NAME and TABLE
+1. Install all dependencies to virtual environment containing VINPUT.
+2. Open [cnx.py](Cnx.py) and update DRIVER_NAME, SERVER_NAME, DATABASE_NAME and TABLE
     attributes to connect to SEAMLESS and the correct table mapping ALGResidualNEWIDs
     to residuals.
-3. Update file ALG_US_CHROMEMAP.csv to most recent version.
-4. Drop project directory into  
+3. Update file ALG_US_CHROMEMAP.csv to most recent version available.
+
+# Usage
