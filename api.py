3ccfe5fc4f19419d4caeb44c132aa4a3e1b974c9
ramirezra4 <51302686+ramirezra4@users.noreply.github.com>
2022-08-09 14:48:51 -0700

    VINPUT operational for enpoints 1 and 2.

--- api.py
+++ api.py
@@ -10,15 +10,22 @@ Web interface for vinput API.
 app = Flask(__name__)
 """Instantiate Flask Web App"""
 
-@app.route('/residuals/vin=<string:vin>&state=<string:state>', methods=['GET'])
-def get_vinmap(vin, state):
-    """Instantiate Vinput Rest API"""
-    return main(vin, state_region[state])
-
 @app.route('/algcode/vin=<string:vin>', methods=['GET'])
 def get_algcode(vin):
+    """
+    Input: VIN
+    Output: ALG code.
+    """
     _algcode = str(vin_alg(vin))
     return jsonify({'ALG Code': _algcode, 'VIN': vin})
+
+@app.route('/residuals/vin=<string:vin>&state=<string:state>&date=<string:date>', methods=['GET'])
+def get_vinmap(vin, state, date):
+    """
+    Input: Vin, State, Contract Date
+    Output: Model Year, Vehicle Descriptions (Make, Model, Style), ALG Codes (Make Model Style), RVs 
+    """
+    return jsonify(main(vin, state_region[state], date))
 
 
 if __name__ == '__main__':
--- platterALG.py
+++ platterALG.py
@@ -1,6 +1,8 @@
+from MMRcall import MMRapi
 import vinwhiz as wiz
 import pandas as pd
 from Cnx import Cnx
+from dateutil import parser
 
 def slice(i, end, start=0):
     """
@@ -11,12 +13,27 @@ def slice(i, end, start=0):
     """
     return str(i)[start:end]
 
-def main(vin, region):
+def main(vin, region, date):
     """
     Main functional interface for VINPUT.
     Input: VIN Number, State (abr).
     Output: Dataframe
     """
+    # fix date format for internal use
+    date = parser.parse(date)
+    # Create Vehicle Descriptions Dict
+    mmr = MMRapi(vin)
+    mmr.match()
+    mmr_make = mmr.make()
+    mmr_model = mmr.model()
+    mmr_style = mmr.body()
+    mmr_yr = mmr.model_year()
+    vehicle_desc = {
+        'Make': mmr_make,
+        'Model': mmr_model,
+        'Style': mmr_style
+    }
+
     algcode = wiz.vin_alg(vin)
     make_number = slice(algcode, 3)
     model_num = slice(algcode, 6, 3)
@@ -25,13 +42,20 @@ def main(vin, region):
     conn = Cnx()
     quer = f"""
         SELECT 
-        [ModelYear],
-        [Description], 
+        [ModelYear], 
         [MakeNumber],
         [ModelNumber],
         [Style],
-        [Region],
-        [EffectiveDate]
+        [Month24],
+        [Month30],
+        [Month36],
+        [Month42],
+        [Month48],
+        [Month54],
+        [Month60],
+        [MRM],
+        [EffectiveDate],
+        [Region]
         FROM [vinput].[dbo].[ALGResidualNewTable20220708]
         WHERE [MakeNumber] = {make_number} AND
         [ModelNumber] = {model_num} AND
@@ -42,6 +66,25 @@ def main(vin, region):
     conn.set_query(quer)
     make_match = conn.execute()
     df = pd.DataFrame(make_match)
-    df.columns = ['Model Year', 'Description', 'Make Number', 'Model Number', 'Style', 'Region', 'Effective Date']
+    df.columns = [
+        'Model Year',
+        'Make Code',
+        'Model Code',
+        'Style Code',
+        'Month 24',
+        'Month 30',
+        'Month 36',
+        'Month 42',
+        'Month 48',
+        'Month 54',
+        'Month 60',
+        'MRM',
+        'Effective Date',
+        'Region'
+    ]
+    
+    dtmdt = lambda y: parser.parse(y)
+    df['Effective Date'] = df['Effective Date'].apply(dtmdt)
+    df = df.drop(df[df['Effective Date'] > date].index)
     df = df[df['Effective Date'] == df['Effective Date'].max()]
-    return df.to_json()
+    return df.iloc[0].to_dict()
